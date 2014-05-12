3687da2e9c857695d100e45ffb6d4ac63641fa43
Ben Wu <wubin28@gmail.com>
2014-05-12 08:43:58 +0800

    Working on TODO-working-on: will the alarm be on if I call the check method. Added test will_the_alarm_be_on_if_I_call_the_check_method() and wrote an assertion.

--- tire-pressure/src/test/java/tire/pressure/AlarmTest.java
+++ tire-pressure/src/test/java/tire/pressure/AlarmTest.java
@@ -1,7 +1,14 @@
 package tire.pressure;
 
+import org.junit.Test;
+
 /**
  * Created by benwu on 14-5-12.
  */
 public class AlarmTest {
+    // TODO-working-on: will the alarm be on if I call the check method
+    @Test
+    public void will_the_alarm_be_on_if_I_call_the_check_method() {
+        assertTrue(alarm.isAlarmOn());
+    }
 }
