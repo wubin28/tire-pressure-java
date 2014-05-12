786a835fd39bc9b62b700a08449a8be40f3e7ee5
Ben Wu <wubin28@gmail.com>
2014-05-12 12:47:47 +0800

    Created field StubSensor.queue.

--- tire-pressure/src/main/java/tire/pressure/StubSensor.java
+++ tire-pressure/src/main/java/tire/pressure/StubSensor.java
@@ -1,10 +1,14 @@
 package tire.pressure;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * Created by benwu on 14-5-12.
  */
 public class StubSensor implements Sensor {
     private double pressureValue;
+    private Queue<Double> queue = new ArrayDeque<Double>();
 
     public void setPressureValueToBeChecked(double pressureValue) {
         this.pressureValue = pressureValue;
