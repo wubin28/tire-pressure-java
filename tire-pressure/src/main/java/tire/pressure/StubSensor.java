2132b6c81705349b5d142bcaf9c434b29e42cb28
Ben Wu <wubin28@gmail.com>
2014-05-12 12:52:16 +0800

    Updated method StubSensor.setPressureValueToBeChecked() to use the queue.

--- tire-pressure/src/main/java/tire/pressure/StubSensor.java
+++ tire-pressure/src/main/java/tire/pressure/StubSensor.java
@@ -7,11 +7,10 @@ import java.util.Queue;
  * Created by benwu on 14-5-12.
  */
 public class StubSensor implements Sensor {
-    private double pressureValue;
     private Queue<Double> queue = new ArrayDeque<Double>();
 
     public void setPressureValueToBeChecked(double pressureValue) {
-        this.pressureValue = pressureValue;
+        this.queue.offer(pressureValue);
     }
 
     @Override
