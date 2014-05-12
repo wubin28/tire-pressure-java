3c58b3d76e36bd230920fd4a861ebe1ceadba2fa
Ben Wu <wubin28@gmail.com>
2014-05-12 12:20:22 +0800

    Added test the_alarm_will_be_on_if_the_pressure_value_is_out_of_range() and made it green.

--- tire-pressure/src/test/java/tire/pressure/AlarmTest.java
+++ tire-pressure/src/test/java/tire/pressure/AlarmTest.java
@@ -9,13 +9,11 @@ import static org.junit.Assert.assertTrue;
  * Created by benwu on 14-5-12.
  */
 public class AlarmTest {
-    // TODO: the alarm will be off if the pressure value is normal
     @Test
     public void the_alarm_will_be_off_if_the_pressure_value_is_normal() {
         // Arrange
         StubSensor stubSensor = new StubSensor();
         stubSensor.setPressureValueToBeChecked(Alarm.LowPressureThreshold);
-        // TODO-working-on: Substitute the RandomSensor with StubSensor
         Alarm alarm = new Alarm(stubSensor);
 
         // Act
@@ -26,5 +24,19 @@ public class AlarmTest {
     }
 
     // TODO: the alarm will be on if the pressure value is out of range
+    @Test
+    public void the_alarm_will_be_on_if_the_pressure_value_is_out_of_range() {
+        // Arrange
+        StubSensor stubSensor = new StubSensor();
+        stubSensor.setPressureValueToBeChecked(Alarm.LowPressureThreshold - 1);
+        Alarm alarm = new Alarm(stubSensor);
+
+        // Act
+        alarm.check();
+
+        // Assert
+        assertTrue(alarm.isAlarmOn());
+    }
+
     // TODO: the alarm will still be on if a normal pressure value comes after an out of range one
 }
